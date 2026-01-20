7beaf777ce19056754c628b4be70ee30d6e55cee
ngocbecon2007 <mduc08758@gmail.com>
2026-01-20 21:32:35 +0700

    Delete buy.php
    Signed-off-by: ngocbecon2007 <mduc08758@gmail.com>

--- buy.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-include 'config.php';
-if(!isset($_SESSION['user'])) die("Login first");
-$id=$_POST['id'];
-$uid=$_SESSION['user']['id'];
-$conn->query("INSERT INTO orders(user_id,service_id,status) VALUES($uid,$id,'pending')");
-echo "Đặt đơn thành công";
-?>
