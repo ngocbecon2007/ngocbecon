91b912ecb3d8ae977a9c3a43b5d6a3e7949b16b9
ngocbecon2007 <mduc08758@gmail.com>
2026-01-20 21:30:45 +0700

    shop-cay-thue-pro
    Signed-off-by: ngocbecon2007 <mduc08758@gmail.com>

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 
-<?php include 'config.php'; ?>
-<!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" href="assets/style.css">
-<title>Shop Cày Thuê PRO</title>
-</head>
-<body>
-<header><h1>🌸 SHOP CÀY THUÊ PRO</h1></header>
-<div class="box">
-<h2>Chào mừng đến shop bán thật</h2>
-<a href="shop.php"><button>Vào Shop</button></a>
-<a href="login.php"><button>Đăng nhập</button></a>
-</div>
-</body>
-</html>
+<?php include '../config.php'; ?>
+<h1>ADMIN DASHBOARD</h1>
+<a href="services.php">Dịch vụ</a> |
+<a href="orders.php">Đơn</a> |
+<a href="deposits.php">Bill</a>
